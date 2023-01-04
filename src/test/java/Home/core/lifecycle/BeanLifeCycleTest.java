37cb46af7848f54d31bb5cf2a894b5416e1a6ef5
elogio.mi <elogio.mi@gmail.com>
2023-01-04 23:16:22 +0900

    part 8 BeanLifeCycle end

---
+++ src/test/java/Home/core/lifecycle/BeanLifeCycleTest.java
@@ -0,0 +1,29 @@
+package Home.core.lifecycle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+public class BeanLifeCycleTest {
+
+    @Test
+    public void lifeCycleTest() {
+        //.close를 위해 applicationcontext 하위 클래스를 사용.
+        ConfigurableApplicationContext ac = new AnnotationConfigApplicationContext(LifeCycleConfig.class);
+        NetworkClient client = ac.getBean(NetworkClient.class);
+        ac.close();
+    }
+
+    @Configuration
+    static class LifeCycleConfig{
+//        @Bean(initMethod = "init", destroyMethod = "close")
+        @Bean
+        public NetworkClient networkClient() {
+            NetworkClient networkClient = new NetworkClient();
+            networkClient.setUrl("http://genshin.kr");
+            return networkClient;
+        }
+    }
+}
